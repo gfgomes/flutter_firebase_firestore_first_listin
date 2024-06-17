17ea00c56d3fac4c15ae6251b402ec3d12143cea
Gabriel Gomes <gabriel.fgomes@gmail.com>
2024-06-17 00:21:32 -0300

    Configura o login como pagina inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_firebase_firestore_first/_core/my_colors.dart';
+import 'package:flutter_firebase_firestore_first/authentication/screens/auth_screen.dart';
 import 'package:flutter_firebase_firestore_first/firestore/presentation/home_screen.dart';
 import 'firebase_options.dart';
 
@@ -50,7 +51,8 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const HomeScreen(),
+      //home: const HomeScreen(),
+      home: const AuthScreen(),
     );
   }
 }
